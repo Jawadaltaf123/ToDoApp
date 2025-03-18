2362038cf3e068ddf347889cfd59ca196bc0ca87
jawad altaf <jawadaltaf004@gmail.com>
2025-03-18 13:26:28 +0500

    coomi dep

--- WebAPI/WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ac1f6d3b69635c2ebbccd225e7e5634cae2c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
